2069b289a9ee76493a85e5113d7981e61bab2b82
Will Walker <williamwalker753@gmail.com>
2021-10-17 01:20:42 -0500

    added app page view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import stopSign from "./stop-sign.png";
+import "./app.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="outer-container">
+      <div className="content-container">
+        <img src={stopSign} alt="stop sign" />
+        <h1>Deceptive site ahead</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Attackers on <strong>iwouldnotclickthislink.ml</strong> may trick 
+          you into doing some crazy shit like installing software or revealing 
+          your personal information (for example, passwords, phone numbers, or credit cards).
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+          <input type="checkbox" name="fake-checkbox" />
+          <label htmlFor="fake-checkbox">
+            &nbsp;
+            Automatically report details of possible security incidents to Foogle.
+            &nbsp;
+            <a href="https://haveibeenpwned.com/" rel="noreferrer"target="_blank">
+              Privacy policy
+            </a>
+          </label>          
+        </form>
+        <div className="flex-row-apart">
+          <a href="https://haveibeenpwned.com/" rel="noreferrer"target="_blank">
+            Details  
+          </a>
+          <a href="https://joebiden.com/presidency-for-all-americans/" rel="noreferrer"target="_blank">
+            <button>
+              Back to safety
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
